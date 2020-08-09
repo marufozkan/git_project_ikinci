9625e2eda18099c885c648e0fed47629a5a41db8
marufozkan <69048132+marufozkan@users.noreply.github.com>
2020-08-09 23:19:52 +0300

    Update Deneme.java

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("ikinci git proje");
 		System.out.println("ikinci satir kodum");
+		System.out.println("remote degisiklik");
 	}
 
 }
